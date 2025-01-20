db1044ec4499122c1bc147baf9a76b55d64d599d
Stefany Cantero <stefanycanteroc@gmail.com>
2025-01-19 21:43:58 -0500

    Update model

--- app.py
+++ app.py
@@ -10,20 +10,89 @@ def index():
 
 @app.route('/form', methods=['POST'])
 def form():
-    model = pickle.load(open('model.pkl', 'rb'))
-    try:
-        if request.method == 'POST':
-            c1 = float(request.form.get('c1'))
-            c2 = float(request.form.get('c2'))
-            c3 = float(request.form.get('c3'))
-            c4 = float(request.form.get('c4'))
-            c5 = float(request.form.get('c5'))
-            c6 = float(request.form.get('c6'))
-            c7 = float(request.form.get('c7'))
 
-            data = np.array([[c1, c2, c3, c4, c5, c6, c7]])
-            prediction = model.predict(data)[0][0]
-            return jsonify(prediction=prediction)
+    """
+    0   loan_amnt                            887379 non-null  float64
+    1   term                                 887379 non-null  int64  
+    2   int_rate                             887379 non-null  float64
+    3   installment                          887379 non-null  float64
+    4   sub_grade                            887379 non-null  float64
+    5   emp_length                           887379 non-null  int32  
+    6   annual_inc                           887379 non-null  float64
+    7   loan_status                          887379 non-null  int32  
+    8   dti                                  887379 non-null  float64
+    9   delinq_2yrs                          887379 non-null  float64
+    10  inq_last_6mths                       887379 non-null  float64
+    11  open_acc                             887379 non-null  float64
+    12  pub_rec                              887379 non-null  float64
+    13  revol_bal                            887379 non-null  float64
+    14  revol_util                           887379 non-null  float64
+    15  collections_12_mths_ex_med           887379 non-null  float64
+    16  acc_now_delinq                       887379 non-null  float64
+    17  tot_coll_amt                         887379 non-null  float64
+    18  tot_cur_bal                          887379 non-null  float64
+    19  home_ownership_MORTGAGE              887379 non-null  float64
+    20  home_ownership_NONE                  887379 non-null  float64
+    21  home_ownership_OTHER                 887379 non-null  float64
+    22  home_ownership_OWN                   887379 non-null  float64
+    23  home_ownership_RENT                  887379 non-null  float64
+    24  verification_status_Source Verified  887379 non-null  float64
+    25  verification_status_Verified         887379 non-null  float64
+    26  purpose_credit_card                  887379 non-null  float64
+    27  purpose_debt_consolidation           887379 non-null  float64
+    28  purpose_educational                  887379 non-null  float64
+    29  purpose_home_improvement             887379 non-null  float64
+    30  purpose_house                        887379 non-null  float64
+    31  purpose_major_purchase               887379 non-null  float64
+    32  purpose_medical                      887379 non-null  float64
+    33  purpose_moving                       887379 non-null  float64
+    34  purpose_other                        887379 non-null  float64
+    35  purpose_renewable_energy             887379 non-null  float64
+    36  purpose_small_business               887379 non-null  float64
+    37  purpose_vacation                     887379 non-null  float64
+    38  purpose_wedding                      887379 non-null  float64
+    """
+
+    model = pickle.load(open('model.pkl', 'rb'))
+    try:            
+        features = [
+            'loan_amnt', 'term', 'int_rate', 'installment', 'sub_grade', 'emp_length',
+            'annual_inc', 'dti', 'delinq_2yrs', 'inq_last_6mths', 'open_acc', 'pub_rec',
+            'revol_bal', 'revol_util', 'collections_12_mths_ex_med', 'acc_now_delinq',
+            'tot_coll_amt', 'tot_cur_bal', 'home_ownership', 'verification_status', 'purpose'
+        ]
+
+        # Guardar las variables ingresadas en un diccionario variable:valor ingresado
+        form_data = {feature: request.form.get(feature) for feature in features}
+
+        for feature in features:
+            if feature not in ['home_ownership', 'verification_status', 'purpose']:
+                form_data[feature] = float(form_data[feature])
+
+        # Variables que pasan por One Hot 
+        categorical_features = {
+            'home_ownership': ['MORTGAGE', 'NONE', 'OTHER', 'OWN', 'RENT'],
+            'verification_status': ['Source Verified', 'Verified'],
+            'purpose': [
+                'credit_card', 'debt_consolidation', 'educational', 'home_improvement', 'house',
+                'major_purchase', 'medical', 'moving', 'other', 'renewable_energy', 'small_business',
+                'vacation', 'wedding'
+            ]
+        }
+
+        encoded_features = []
+
+        # La opción elegida en cada categoría se codifica como 1 y el resto como 0
+        for feature, value in categorical_features.items():
+            for category in value:
+                encoded_features.append(1 if form_data[feature] == category else 0)
+
+        data = np.array([list(form_data.values())[:17] + encoded_features])
+
+        prediction = model.predict(data)[0]    
+
+        return jsonify(prediction=prediction)
+
     except Exception as e:
         return jsonify(error=str(e))
 
